c5e191dfb5d6b9428daded595d42401acccdc89e
Kevin Downs <kevin.downs04@gmail.com>
2019-11-06 21:54:28 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,7 @@ end
 
 def monopoly_with_third_tier
   monopoly = {
-    :railroads => {:pieces => 4, :names => {}, :rent_in_dollars => {}}
+    :railroads => {:pieces => 4, :names => {}, :rent_in_dollars => {}
   }
   monopoly[:railroads][:rent_in_dollars][:one_piece_owned] = 25
   monopoly[:railroads][:rent_in_dollars][:two_pieces_owned] = 50
